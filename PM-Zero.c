d92f5ba796c726f35d88bc97d72d8a432e1d0fd4
joshua lee franco <joshua.lee.franco@gmail.com>
2015-09-21 01:13:50 -0400

    Initial commit of PM-Zero file

---
+++ PM-Zero.c
@@ -0,0 +1,44 @@
+//Joshua Lee Franco
+//Fall 2015 System Softwares
+//Assignment 1- PM\O VM
+//Due 9/20/15
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_STACK_HEIGHT 2000
+#define MAX_CODE_LENGTH 500
+#define MAX_LEXI_LEVELS 3
+
+struct instructions
+{
+	int op;
+	int l;
+	int m;
+};
+
+
+int main(int argc, char const *argv[])
+{
+	
+	int SP = 0;
+	int BP = 1;
+	int PC = 0;
+	int IR = 0;
+
+
+
+
+	return 0;
+}
+
+int base(l, base) {
+    int b1 = base; // find base L levels down
+    while (l > 0) {
+        b1 = stack[b1 + 1];
+        l--;
+    }
+    return b1;
+}
